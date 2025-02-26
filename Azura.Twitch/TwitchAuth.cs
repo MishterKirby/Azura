f2dfb27849460e01f4461e8130c7a8707c505048
MishterKirby <48867925+MishterKirby@users.noreply.github.com>
2025-02-26 17:22:58 -0600

    Begin adding OAuth code

---
+++ Azura.Twitch/TwitchAuth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azura.Azura.Twitch
+{ 
+    //Fill this in with the information in your Twitch dev console
+
+    public class TwitchAuth
+    {
+        public static readonly string TwitchID = "fill";
+        public static readonly string TwitchRedirectUri = "this";
+        public static readonly string TwitchClientSecret = "in";
+    }
+}
