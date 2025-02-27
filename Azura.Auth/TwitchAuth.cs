9a0e4567864e4bb28952a33934f9c7db682a449a
MishterKirby <48867925+MishterKirby@users.noreply.github.com>
2025-02-27 00:11:57 -0600

    Create TwitchAuth.cs

---
+++ Azura.Auth/TwitchAuth.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azura.Twitch
+{
+    public class TwitchAuth
+    {
+        public static readonly string chatUsername = ""; //username that will act as bot
+        public static readonly string streamerChannel = ""; //channel you want the bot to connect to
+
+
+        public static readonly string TwitchAppID = "";  //Client ID from Twitch Console
+        public static readonly string TwitchRedirectUri = ""; //Redirect URI from Twitch Console
+        public static readonly string TwitchClientSecret = ""; //Secret from Twitch Console
+    }
+}
