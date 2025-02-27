1509820b8c07b84a95ae46c8ca221ce4ab228491
MishterKirby <48867925+MishterKirby@users.noreply.github.com>
2025-02-27 00:17:26 -0600

    Update TwitchAuth.cs

--- Azura.Twitch/TwitchAuth.cs
+++ Azura.Twitch/TwitchAuth.cs
@@ -1,17 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Azura.Azura.Twitch
-{ 
-    //Fill this in with the information in your Twitch dev console
-
+namespace Azura.Twitch
+{
     public class TwitchAuth
     {
-        public static readonly string TwitchID = "fill";
-        public static readonly string TwitchRedirectUri = "this";
-        public static readonly string TwitchClientSecret = "in";
+        public static readonly string chatUsername = ""; //username that will act as bot
+        public static readonly string streamerChannel = ""; //channel you want the bot to connect to
+
+
+        public static readonly string TwitchAppID = "";  //Client ID from Twitch Console
+        public static readonly string TwitchRedirectUri = ""; //Redirect URI from Twitch Console
+        public static readonly string TwitchClientSecret = ""; //Secret from Twitch Console
     }
 }
